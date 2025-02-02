930e2516aa6c2ccc7b621fc2833c8bacb8f9bac6
BugHunter-X <askzenai@gmail.com>
2025-02-02 17:36:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// This code uses Tailwind CSS classes to style a button, but the classes are not applied correctly.
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Click Me
+</button>
+```
